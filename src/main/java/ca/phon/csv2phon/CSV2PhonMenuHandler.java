bcb3e96b2080df682f27227651e0b3855a9bf9d7
Gregory Hedlund <greg.hedlund@gmail.com>
2014-06-02 18:16:24 -0230

    Added menu item for csv import

---
+++ src/main/java/ca/phon/csv2phon/CSV2PhonMenuHandler.java
@@ -0,0 +1,73 @@
+package ca.phon.csv2phon;
+
+import java.awt.Window;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+import ca.phon.csv2phon.wizard.CSVImportWizard;
+import ca.phon.plugin.IPluginExtensionFactory;
+import ca.phon.plugin.IPluginExtensionPoint;
+import ca.phon.plugin.IPluginMenuFilter;
+import ca.phon.plugin.PhonPlugin;
+import ca.phon.project.Project;
+import ca.phon.ui.CommonModuleFrame;
+import ca.phon.ui.action.PhonActionEvent;
+import ca.phon.ui.action.PhonUIAction;
+
+@PhonPlugin
+public class CSV2PhonMenuHandler 
+	implements IPluginMenuFilter, IPluginExtensionPoint<IPluginMenuFilter> {
+
+	@Override
+	public Class<?> getExtensionType() {
+		return IPluginMenuFilter.class;
+	}
+
+	@Override
+	public IPluginExtensionFactory<IPluginMenuFilter> getFactory() {
+		return new IPluginExtensionFactory<IPluginMenuFilter>() {
+			
+			@Override
+			public IPluginMenuFilter createObject(Object... args) {
+				return CSV2PhonMenuHandler.this;
+			}
+			
+		};
+	}
+
+	@Override
+	public void filterWindowMenu(Window owner, JMenuBar menuBar) {
+		if(!(owner instanceof CommonModuleFrame)) return;
+		final CommonModuleFrame cmf = (CommonModuleFrame)owner;
+		final Project project = cmf.getExtension(Project.class);
+		
+		JMenu pluginsMenu = null;
+		for(int i = 0; i < menuBar.getMenuCount(); i++) {
+			final JMenu menu = menuBar.getMenu(i);
+			if(menu.getText().equals("Plugins")) {
+				pluginsMenu = menu;
+				break;
+			}
+		}
+		
+		if(pluginsMenu != null) {
+			final PhonUIAction csv2PhonAct = new PhonUIAction(this, "cvs2PhonWizard", cmf);
+			csv2PhonAct.putValue(PhonUIAction.NAME, "Import from CSV...");
+			csv2PhonAct.putValue(PhonUIAction.SHORT_DESCRIPTION, "Import CSV files as new Sessions in the current project.");
+			pluginsMenu.add(csv2PhonAct);
+		}
+	}
+	
+	public void cvs2PhonWizard(PhonActionEvent pae) {
+		final CommonModuleFrame cmf = (CommonModuleFrame)pae.getData();
+		final Project project = cmf.getExtension(Project.class);
+		if(project == null) return;
+		
+		final CSVImportWizard wizard = new CSVImportWizard(project);
+		wizard.pack();
+		wizard.centerWindow();
+		wizard.showWizard();
+	}
+
+}
